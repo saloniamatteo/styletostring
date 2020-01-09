7990fd5ce1ff537f575ba0c2dd15ecd60ded8d59
GoDzM4TT3O <28765699+GoDzM4TT3O@users.noreply.github.com>
2020-01-09 16:11:51 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# styletostring
+This is a little script I made in bash that helps you add any style you like
+to a string. It's currently the first stable working version, so I'll just
+publish it for now.
+Because of this, if you want to add another modifier to your string,
+I recommend adding it manually; for example: I have a string with a blue background
+('\e[44mtest') and I want to make it bold, in that case I will add the number 1.
+The result will be '\e[1;44mtest'.
+
+Any contribution will be appreciated!
+
+## Resources
+To find more modifiers, please use this table:
+
+#-------------------------+--------------------------------+
+# Text Format      |  Text color        | Background color |     
+#-----------+-------------+--------------+-----------------+
+0: normal text     | 30: black          | 40: black        |
+1: bold text       | 31: red            | 41: red          |
+2: darker text     | 32: green          | 42: green        |
+3: italic text     | 33: yellow         | 43: yellow       |
+4: underlined text | 34: blue           | 44: blue         |
+                   | 35: purple         | 45: purple       |
+                   | 36: cyan           | 46: cyan         |
+                   | 37: white          | 47: white        |
+#----------------------------------------------------------+
+
+You can also see vaniacer's table containing more color codes: https://github.com/vaniacer/bash_color/blob/master/color
---
+++ styletostring.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+clear
+# Print version info
+printf "\e[1;37;44mGoDzM4TT3O\'s styletostring v1.0\e[0;37m\n"
+# List of available styles
+colors=("",
+        'Bold text'
+        'Underlined text'
+	'Darker text'
+	'Italic text'
+        'Black foreground'
+        'Red foreground'
+        'Green foreground'
+        'Yellow foreground'
+        'Blue foreground'
+        'Purple foreground'
+        'Cyan foreground'
+        'White foreground'
+        'Black background'
+        'Red background'
+        'Green background'
+        'Yellow background'
+        'Blue background'
+        'Purple background'
+        'Cyan background'
+        'White background')
+# Color codes, each to apply to their relative style name (see README.md)
+codes=("", '1' '4' '2' '3' '30' '31' '32' '33' '34' '35' '36' '0;37;40'\
+ '39' '41' '42' '43' '44' '45' '46' '0;30;47')
+# Ask user for a string
+read -p 'Please type a string: ' string
+echo "Please select a modifier from this list by typing the modifier's number."
+# Print the list of the available styles
+for i in {1..18} ; do
+	printf '\e[%sm[%d] %s\e[m\n' "${codes[i]}" $i "${colors[i]}"
+done;
+
+read -p 'Please select a number: ' number
+# If the second user input is not a number between 1 and 18, do not continue
+if ! [[ $number =~ ^([1-9]|1[0-8])$ ]]
+then
+        echo 'Please only select a number from 1 to 18'
+else
+	# If the second input was a number, print the appropriate style
+	printf '\e[%sm%s\e[m\n' "${codes[number]}" "$string"
+	# Also print the code that was used to add a style to the string
+	echo "printf \e[${codes[number]}m$string"
+	echo "The line above is the command used to reproduce\
+ the style applied to your string; for more info, see README.md"
+fi
+# EOF
